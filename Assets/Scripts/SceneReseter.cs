035e987b9f250c6c416562f2e6e7a8438521709d
miguel <miguelcok27@gmail.com>
2022-01-22 14:38:34 -0500

    FEAT: implement smooth scene transition

---
+++ Assets/Scripts/SceneReseter.cs
@@ -0,0 +1,18 @@
+﻿using DaysSystem;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SceneReseter : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector cinematic;
+
+    public void Reset()
+    {
+        cinematic.Play();
+    }
+
+    public void PassDay()
+    {
+        DayData.Instance.AddDayAndRestart();
+    }
+}
