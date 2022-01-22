3bab72b46b9331d667bb5e875d02d57593d35ecf
miguel <miguelcok27@gmail.com>
2022-01-22 07:39:22 -0500

    FEAT: implement day dependant script

---
+++ Assets/Scripts/DaysSystem/DayDependant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DaysSystem
+{
+    public class DayDependant : MonoBehaviour
+    {
+        [SerializeField] private List<int> days;
+
+        private void Start()
+        {
+            if (!days.Contains(DayData.Instance.Day))
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
