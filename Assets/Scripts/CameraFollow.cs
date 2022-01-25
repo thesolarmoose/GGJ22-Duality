416976b5863d79c4ad79f9fbec3f4c3659a66cb9
miguel <miguelcok27@gmail.com>
2022-01-25 10:29:51 -0500

    FIX: fix camera bounds

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -8,6 +9,14 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private Rect bounds;
 
+    private Camera _camera;
+    private Rect _positionBounds;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
     private void LateUpdate()
     {
         MoveTowards();
@@ -15,13 +24,29 @@ public class CameraFollow : MonoBehaviour
 
     private void MoveTowards()
     {
+        ComputePositionBounds();
         var targetPosition = target.position;
         var selfPosition = transform.position;
 
         Vector3 newPosition = Vector2.Lerp(selfPosition, targetPosition, speed);
         newPosition.z = selfPosition.z;
-        newPosition.x = Mathf.Clamp(newPosition.x, bounds.xMin, bounds.xMax);
-        newPosition.y = Mathf.Clamp(newPosition.y, bounds.yMin, bounds.yMax);
+        newPosition.x = Mathf.Clamp(newPosition.x, _positionBounds.xMin, _positionBounds.xMax);
+        newPosition.y = Mathf.Clamp(newPosition.y, _positionBounds.yMin, _positionBounds.yMax);
         transform.position = newPosition;
+    }
+
+    private void ComputePositionBounds()
+    {
+        float aspect = _camera.aspect;
+        float height = _camera.orthographicSize * 2;
+        float halfHeight = height * 0.5f;
+        float width = height * aspect;
+        float halfWidth = width * 0.5f;
+
+        float xMin = bounds.xMin + halfWidth;
+        float xMax = bounds.xMax - halfWidth;
+        float yMin = bounds.yMin + halfHeight;
+        float yMax = bounds.yMax - halfHeight;
+        _positionBounds = new Rect(xMin, yMin, xMax - xMin, yMax - yMin);
     }
 }
