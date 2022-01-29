c6f3e8f168a594a2b71e61bf05ccdd32b5c234da
miguel <miguelcok27@gmail.com>
2022-01-29 13:52:16 -0500

    FEAT: close game with escape in credits menu

---
+++ Assets/Scripts/CloseGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CloseGame : MonoBehaviour
+{
+    [SerializeField] private InputAction closeButton;
+
+    private void Start()
+    {
+        closeButton.Enable();
+        closeButton.performed += context => Application.Quit();
+    }
+
+    private void OnEnable()
+    {
+        closeButton?.Enable();
+    }
+
+    private void OnDisable()
+    {
+        closeButton?.Disable();
+    }
+}
