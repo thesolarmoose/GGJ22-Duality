999d1e2f8f81e5eb7e1fcd5c1c1a26cd16f3546e
miguel <miguelcok27@gmail.com>
2022-02-04 09:01:57 -0500

    FEAT: add interaction with bookshelf to change language

---
+++ Assets/Scripts/LanguageChanger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+public class LanguageChanger : MonoBehaviour
+{
+    [SerializeField] private List<Locale> locales;
+
+    private int _currentIndex;
+
+    private void Start()
+    {
+        SetIndexOfCurrentLocale();
+    }
+
+    public void ChangeLanguage()
+    {
+        _currentIndex = (_currentIndex + 1) % locales.Count;
+        LocalizationSettings.Instance.SetSelectedLocale(locales[_currentIndex]);
+    }
+
+    private void SetIndexOfCurrentLocale()
+    {
+        var currentLocale = LocalizationSettings.Instance.GetSelectedLocale();
+        _currentIndex = locales.IndexOf(currentLocale);
+    }
+}
