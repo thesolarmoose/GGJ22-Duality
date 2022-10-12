3344bfe794df5f507154133c21ff1782df554ba8
mnicolas94 <miguelcok27@gmail.com>
2022-10-12 15:42:35 -0400

    FEAT: pause menu

---
+++ Assets/Scripts/UI/PauseMenuLauncher.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using AsyncUtils;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace UI
+{
+    public class PauseMenuLauncher : MonoBehaviour
+    {
+        [SerializeField] private List<PopUpMenu> _puzzleMenus;
+        [SerializeField] private AsyncPopup _pauseMenuPopup;
+        [SerializeField] private InputAction _pauseAction;
+
+        [SerializeField] private UnityEvent _onBeforePause;
+        [SerializeField] private UnityEvent _onAfterPause;
+
+        private CancellationTokenSource _cts;
+        private bool _paused;
+
+        private async void ShowPauseMenu(InputAction.CallbackContext ctx)
+        {
+            bool allPuzzlesDisabled = _puzzleMenus.TrueForAll(
+                puzzle => !puzzle.IsShowing || !puzzle.enabled || !puzzle.gameObject.activeInHierarchy);
+            
+            if (!_paused && allPuzzlesDisabled)
+            {
+                _paused = true;
+                _onBeforePause.Invoke();
+                var ct = _cts.Token;
+                await Popups.ShowPopup(_pauseMenuPopup, ct);    
+                _paused = false;
+                _onAfterPause.Invoke();
+            }
+        }
+
+        private void Start()
+        {
+            _pauseAction.Enable();
+            _pauseAction.performed += ShowPauseMenu;
+        }
+
+        private void OnEnable()
+        {
+            _cts = new CancellationTokenSource();
+            _pauseAction?.Enable();
+        }
+
+        private void OnDisable()
+        {
+            if (!_cts.IsCancellationRequested)
+            {
+                _cts.Cancel();
+            }
+            _cts.Dispose();
+            _pauseAction?.Disable();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PauseMenuPopup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AsyncUtils;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class PauseMenuPopup : AsyncPopup
+    {
+        [SerializeField] private Button _resumeButton;
+        [SerializeField] private Button _quitButton;
+
+        private InputAction _resumeAction;
+
+        public override void Initialize()
+        {
+            _quitButton.onClick.AddListener(Application.Quit);
+            
+            // TODO get from utils
+            var key = Key.Escape;
+            _resumeAction = new InputAction(key.ToString());
+            _resumeAction.AddBinding(Keyboard.current[key]);
+            _resumeAction.Enable();
+        }
+
+        public override async Task Show(CancellationToken ct)
+        {
+            // avoid listening to the same Escape key press that launched the popup and, hence, close immediately
+            await Task.Yield();
+            await Task.Yield();
+            
+            var resumeKeyTask = AsyncUtils.Utils.WaitForInputAction(_resumeAction, ct);
+            var resumeButtonTask = AsyncUtils.Utils.WaitPressButtonAsync(_resumeButton, ct);
+            await Task.WhenAny(resumeButtonTask, resumeKeyTask);
+        }
+
+        private void OnDisable()
+        {
+            _resumeAction?.Dispose();
+        }
+    }
+}
