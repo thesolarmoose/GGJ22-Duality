357d08e58980f30db1b67a573c33e177fa82eac8
mnicolas94 <miguelcok27@gmail.com>
2023-01-10 11:16:28 -0500

    feat: highlight 3rd puzzle's door handle when any cursor (robot or hand) is hovering it

---
+++ Assets/Scripts/Puzzles/HandCursorEnterExitTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Puzzles
+{
+    public class HandCursorEnterExitTrigger : MonoBehaviour
+    {
+        [SerializeField] private HandCursor _cursor;
+        [SerializeField] private Collider2D _collider;
+        [SerializeField] private UnityEvent _onEnter;
+        [SerializeField] private UnityEvent _onExit;
+
+        private bool _lastFrameWasInside;
+        
+        private void Update()
+        {
+            var position = _cursor.GetPosition();
+            bool isInside = IsInsideCollider(position);
+
+            if (_lastFrameWasInside ^ isInside)
+            {
+                var @event = isInside ? _onEnter : _onExit;
+                @event.Invoke();
+            }
+
+            _lastFrameWasInside = isInside;
+        }
+        
+        private bool IsInsideCollider(Vector2 position)
+        {
+            var hit = Physics2D.Raycast(position, Vector2.zero);
+            bool inside = hit.transform == _collider.transform;
+            return inside;
+        }
+    }
+}
