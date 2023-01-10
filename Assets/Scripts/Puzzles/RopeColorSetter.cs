5647aa404799b8d14c9ab21e32d74f2e00b64a6c
mnicolas94 <miguelcok27@gmail.com>
2023-01-10 10:41:56 -0500

    feat: highlight 2nd puzzle's cables when hovered by pointer

---
+++ Assets/Scripts/Puzzles/RopeColorSetter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Puzzles
+{
+    public class RopeColorSetter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private LineRenderer _lineRenderer;
+        [SerializeField] private Color _enterColor;
+        [SerializeField] private Color _exitColor;
+
+        private void SetColor(Color color)
+        {
+            _lineRenderer.startColor = color;
+            _lineRenderer.endColor = color;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            SetColor(_enterColor);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            SetColor(_exitColor);
+        }
+    }
+}
