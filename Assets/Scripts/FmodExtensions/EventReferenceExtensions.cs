91e1e7d42dccfec2ad8f6fa87d2a9d9af077a46c
mnicolas94 <miguelcok27@gmail.com>
2023-01-11 13:19:13 -0500

    feat: implement fmod's event reference extensions

---
+++ Assets/Scripts/FmodExtensions/EventReferenceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using FMODUnity;
+
+namespace FmodExtensions
+{
+    public static class EventReferenceExtensions
+    {
+        public static void PlayEvent(this EventReference @event)
+        {
+            FMODUnity.RuntimeManager.CreateInstance(@event).start();
+        }
+    }
+}
