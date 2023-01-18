88ea0420646bb907793b83a0bb58901ac0cb8fc6
mnicolas94 <miguelcok27@gmail.com>
2023-01-18 12:32:38 -0500

    feat: implement fmod's extension method to play event through context menu

---
+++ Assets/Scripts/FmodExtensions/EmitterMenuItems.cs
@@ -0,0 +1,42 @@
+﻿using FMODUnity;
+using UnityEditor;
+
+namespace FmodExtensions
+{
+    public static class EmitterMenuItems
+    {
+        [MenuItem("GameObject/FMOD Extensions/Play emitter's event")]
+        public static void PlayEmitter()
+        {
+            bool onlyOneSelected = Selection.count == 1;
+            var gameObject = Selection.activeGameObject;
+            bool isGameObject = gameObject != null;
+            if (onlyOneSelected && isGameObject)
+            {
+                bool hasAudioEmitter = gameObject.TryGetComponent<StudioEventEmitter>(out var emitter);
+                if (hasAudioEmitter)
+                {
+                    emitter.Play();
+                }
+            }
+        }
+        
+        [MenuItem("GameObject/FMOD Extensions/Play emitter's event", true)]
+        public static bool PlayEmitterValidation()
+        {
+            bool onlyOneSelected = Selection.count == 1;
+            var gameObject = Selection.activeGameObject;
+            bool isGameObject = gameObject != null;
+            if (onlyOneSelected && isGameObject)
+            {
+                bool hasAudioEmitter = gameObject.TryGetComponent<StudioEventEmitter>(out var _);
+                if (hasAudioEmitter)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
