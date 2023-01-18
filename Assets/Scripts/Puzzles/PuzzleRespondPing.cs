e8ec0e23fc28cc7037101f3c53a3c7890cac036a
mnicolas94 <miguelcok27@gmail.com>
2023-01-18 14:37:38 -0500

    feat: remove audio sources from places where FMOD it's being used now

--- Assets/Scripts/Puzzles/PuzzleRespondPing.cs
+++ Assets/Scripts/Puzzles/PuzzleRespondPing.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Audio;
+﻿using System.Collections.Generic;
 using FmodExtensions;
 using FMODUnity;
 using InputActions;
--- Assets/Scripts/Puzzles/PuzzlesConnectCables.cs
+++ Assets/Scripts/Puzzles/PuzzlesConnectCables.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using Audio;
 using FmodExtensions;
 using FMODUnity;
 using InputActions;
