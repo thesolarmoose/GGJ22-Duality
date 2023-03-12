a6d8e3899ebfc43e8a6554cd0e13694b1c53df67
mnicolas94 <miguelcok27@gmail.com>
2023-03-12 15:34:19 -0400

    feat(analytics): trigger solved puzzles events

---
+++ Assets/Scripts/Services/Analytics/PuzzleSolvedEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using DaysSystem;
+using UnityEngine;
+using Utils.Attributes;
+
+namespace Services.Analytics
+{
+    [CreateAssetMenu(fileName = "PuzzleSolvedEvent", menuName = "Analytics/CustomEvents/PuzzleSolvedEvent", order = 0)]
+    public class PuzzleSolvedEvent : ScriptableObject, ICustomEvent
+    {
+        [SerializeField] private int _number;
+
+        [SerializeField, AutoProperty(AutoPropertyMode.Asset)]
+        private DayData _dayData;
+        
+        public string GetEventName()
+        {
+            return "puzzle_solved";
+        }
+
+        public IDictionary<string, object> GetEventParams()
+        {
+            return new Dictionary<string, object>
+            {
+                {"number", _number},
+                {"day", _dayData.Day}
+            };
+        }
+    }
+}
