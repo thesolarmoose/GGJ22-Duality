89658fc25b9376665300532651060c6818f42534
mnicolas94 <miguelcok27@gmail.com>
2023-03-15 17:30:20 -0400

    feat: show proper control (based on current control scheme) in tutorials

---
+++ Assets/Scripts/UI/SchemeSpriteAtlasProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Utils.Input;
+
+namespace UI
+{
+    [CreateAssetMenu(fileName = "SchemeSpriteAtlasProvider", menuName = "SchemeSpriteAtlasProvider", order = 0)]
+    public class SchemeSpriteAtlasProvider : ScriptableObject
+    {
+        private static readonly Dictionary<string, string> SchemesToAtlasMap = new Dictionary<string, string>
+        {
+            { "Gamepad", "xbox" },
+            { "Keyboard&Mouse", "desktop" }
+        };
+
+        public string SchemeAtlas => SchemesToAtlasMap[InputSchemeObserverAsset.Instance.CurrentScheme.name];
+    }
+}
